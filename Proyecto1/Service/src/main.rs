d91f710a9884cbee84640e1cdb52cde090239c31
Brandon Tejaxun <brandonteja02@gmail.com>
2024-09-08 20:48:30 -0600

    [update] kernel & servicio rust

--- Proyecto1/Service/src/main.rs
+++ Proyecto1/Service/src/main.rs
@@ -1,13 +1,16 @@
-use std::{fs::File, io::{self, Read}, path::{Path, PathBuf}};
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
 use serde::{Deserialize, Serialize};
 
+// CREACIÓN DE STRUCT
 #[derive(Debug, Serialize, Deserialize)]
 struct SystemInfo {
     #[serde(rename = "Processes")]
     processes: Vec<Process>
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Process {
     #[serde(rename = "PID")]
     pid: u32,
@@ -18,29 +21,136 @@ struct Process {
     #[serde(rename = "MemoryUsage")]
     memory_usage: f64,
     #[serde(rename = "CPUUsage")]
-    cpuy_usage: f64,
+    cpu_usage: f64,
 }
 
+#[derive(Debug, Serialize, Clone)]
+struct LogProcess {
+    pid: u32,
+    container_id: String,
+    name: String,
+    memory_usage: f64,
+    cpu_usage: f64,
+}
+
+// IMPLEMENTACIÓN DE MÉTODOS
 impl Process {
-    fn get_container_id(&self) -> String {
+    fn get_container_id(&self) -> &str {
         let parts: Vec<&str> = self.cmd_line.split_whitespace().collect();
-        for(i, part) in parts.iter().enumerate() {
+        for (i, part) in parts.iter().enumerate() {
             if *part == "-id" {
                 if let Some(id) = parts.get(i + 1) {
-                    return id.to_string();
+                    return id;
                 }
             }
         }
-        
+        "N/A"
     }
 }
 
-fn read_proc_file(file_name: &str) -> Result<String, io::Error> {
-    let path: PathBuf = Path::new("/proc").join(file_name);
+// IMPLEMENTACIÓN DE TRAITS
+impl Eq for Process {}  
+
+impl Ord for Process {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.cpu_usage.partial_cmp(&other.cpu_usage).unwrap_or(std::cmp::Ordering::Equal)
+            .then_with(|| self.memory_usage.partial_cmp(&other.memory_usage).unwrap_or(std::cmp::Ordering::Equal))
+    }
+}
+
+impl PartialOrd for Process {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+// FUNCIONES
+
+fn kill_container(id: &str) -> std::process::Output {
+    let  output = std::process::Command::new("sudo")
+        .arg("docker")
+        .arg("stop")
+        .arg(id)
+        .output()
+        .expect("failed to execute process");
+
+    println!("Matando contenedor con id: {}", id);
+
+    output
+}
+
+fn analyzer( system_info:  SystemInfo) {
+    let mut log_proc_list: Vec<LogProcess> = Vec::new();
+    let mut processes_list: Vec<Process> = system_info.processes;
+
+    processes_list.sort();
+    let (lowest_list, highest_list) = processes_list.split_at(processes_list.len() / 2);
+
+    println!("Bajo consumo");
+    for process in lowest_list {
+        println!("PID: {}, Name: {}, container ID: {}, Memory Usage: {}, CPU Usage: {}", process.pid, process.name, process.get_container_id(), process.memory_usage, process.cpu_usage);
+    }
+
+    println!("------------------------------");
+
+    println!("Alto consumo");
+    for process in highest_list {
+        println!("PID: {}, Name: {}, Icontainer ID {}, Memory Usage: {}, CPU Usage: {}", process.pid, process.name,process.get_container_id(),process.memory_usage, process.cpu_usage);
+    }
+
+    println!("------------------------------");
+    if lowest_list.len() > 3 {
+        // Iteramos sobre los procesos en la lista de bajo consumo.
+        for process in lowest_list.iter().skip(3) {
+            let log_process = LogProcess {
+                pid: process.pid,
+                container_id: process.get_container_id().to_string(),
+                name: process.name.clone(),
+                memory_usage: process.memory_usage,
+                cpu_usage: process.cpu_usage,
+            };
+    
+            log_proc_list.push(log_process.clone());
+
+            // Matamos el contenedor.
+            let _output = kill_container(&process.get_container_id());
+
+        }
+    } 
+
+    if highest_list.len() > 2 {
+        // Iteramos sobre los procesos en la lista de alto consumo.
+        for process in highest_list.iter().take(highest_list.len() - 2) {
+            let log_process = LogProcess {
+                pid: process.pid,
+                container_id: process.get_container_id().to_string(),
+                name: process.name.clone(),
+                memory_usage: process.memory_usage,
+                cpu_usage: process.cpu_usage
+            };
+            log_proc_list.push(log_process.clone());
+            // Matamos el contenedor.
+            let _output = kill_container(&process.get_container_id());
+
+        }
+    }
+
+    // TODO: ENVIAR LOGS AL CONTENEDOR REGISTRO
+    // Hacemos un print de los contenedores que matamos.
+    println!("Contenedores matados");
+    for process in log_proc_list {
+        println!("PID: {}, Name: {}, Container ID: {}, Memory Usage: {}, CPU Usage: {} ", process.pid, process.name, process.container_id,  process.memory_usage, process.cpu_usage);
+    }
+    println!("------------------------------");
+}
+
+fn read_proc_file(file_name: &str) -> io::Result<String> {
+    let path  = Path::new("/proc").join(file_name);
     let mut file = File::open(path)?;
     let mut content = String::new();
-    file.read_to_string(&mut content)?;
 
+    file.read_to_string(&mut content)?;
     Ok(content)
 }
 
@@ -50,15 +160,17 @@ fn parse_proc_to_struct(json_str: &str) -> Result<SystemInfo, serde_json::Error>
 }
 
 fn main() {
-    let json_str = read_proc_file("sysinfo_202112030").unwrap();
-    let system_info = parse_proc_to_struct(&json_str);
+    loop {
+        let system_info: Result<SystemInfo, _>;
+        let json_str = read_proc_file("sysinfo_202112030").unwrap();
+        system_info = parse_proc_to_struct(&json_str);
 
-    match system_info {
-        Ok(info) => {
-            println!("System Info: {:?}", info);
-        },
-        Err(e) => {
-            println!("Error en el parser: {:?}", e);
+        match system_info {
+            Ok( info) => {
+                analyzer(info);
+            }
+            Err(e) => println!("Failed to parse JSON: {}", e),
         }
+        std::thread::sleep(std::time::Duration::from_secs(10));
     }
 }
